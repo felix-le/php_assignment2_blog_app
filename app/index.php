eaa92c387ca9a61794f7e26797c89f67664381a0
Viet Anh Le <mc.vietanh@gmail.com>
2021-04-10 16:28:24 -0400

    done admin roles

--- app/index.php
+++ app/index.php
@@ -2,10 +2,15 @@
   $title = 'Home';
   include './includes/header.php';
 ?>
+<main>
   <h1>Hello World!</h1>
+  <?php if (isset($_GET['error'])) { ?>
+      <p class="error"><?php echo $_GET['error']; ?></p>
+    <?php } ?>
   <?php include 'includes/navbar.php'; ?>
 
   <?php include 'includes/footer.php'; ?>
   <?php include 'includes/scripts.php'; ?>
+  </main>
 </body>
 </html>
